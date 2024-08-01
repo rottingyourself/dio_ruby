b84faf5d1f67bc667b591b2dbf7ed970513b190a
Adriel Rodrigues <120425687+rottingyourself@users.noreply.github.com>
2024-08-01 15:10:24 -0300

    app_estoque_console
    Novos elementos, parte inicial do projeto

--- ruby/basicos/condicionais.rb
+++ ruby/basicos/condicionais.rb
@@ -1,18 +1,49 @@
-puts "Digite um número:"
-numero = gets.to_i
+puts "Digite um número"
+numero1 = gets.to_i
 
-# if numero == 1
-#   puts "Parabéns você acertou o número desejado (1)"
+# puts "Digite um número"
+# numero2 = gets.to_i
 
-# elsif numero == 2
-#   puts "Parabéns você acertou o número desejado (2)" 
+# puts "Digite um número"
+# numero3 = gets.to_i
 
-# else 
-#   puts "Não acertou"  
+
+# puts "Deu certo" if (numero1 == numero2) || (numero2 > numero3)
+# puts "Deu certo" if (numero1 == numero2) && (numero2 > numero3)
+
+# if numero1 == 1
+#     puts "Parabéns você acertou o número desejado (1)"
 # end
 
-unless numero == 1
-  puts "O que você digitou não é o número 1 é o número: #{numero}"
+# puts "Parabéns você acertou o número desejado (1)" if numero1 == 1
+
+# if ( numero1 == 1) 
+#     puts "Parabéns você acertou o número desejado (1)"; 
+# else
+#     puts "Não é um número desejado"; 
+# end
+
+# if ( numero1 == 1) 
+#     puts "Parabéns você acertou o número desejado (1)"; 
+# elsif numero1 == 2
+#     puts "Parabéns você acertou o outro número desejado (2)"; 
+# else
+#     puts "Não é um número desejado"; 
+# end
+
+# unless numero1 == 1
+#     puts "O que você digitou não é o numero 1 é o numero1: #{numero1}"
+# else
+#     puts 'Você digitou o numero1 correto: ' + numero1.to_s
+# end
+
+
+
+case numero1
+when 1
+    puts "Achou o numero: #{numero1}"
+when 2
+    puts "Achou o segundo numero: #{numero1}"
 else
-  puts "Você digitou o número correto: " + numero.to_s
+    puts "Você não acertou: #{numero1}"
 end
